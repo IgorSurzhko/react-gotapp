efa8bd83ccbf53154ea8cdf3bbfc72c01c708108
Igor <ermygut@gmail.com>
2021-03-23 13:22:31 +0200

    show random character, spinner while load

---
+++ src/components/spinner/spinner.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './spinner.css'
+
+const Spinner = () => {
+    return (
+        <div class="loadingio-spinner-spinner-ru50gdpsf5"><div class="ldio-bagtmghhalo">
+<div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div>
+</div></div>
+    )
+}
+
+export default Spinner;
+
