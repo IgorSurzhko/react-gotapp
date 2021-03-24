13d85f0ec84d8ba2746cc8dbeba8a6f0ade84bb5
Igor <ermygut@gmail.com>
2021-03-24 17:31:13 +0200

    bug fix

--- src/components/pages/booksPage.js
+++ src/components/pages/booksPage.js
@@ -5,38 +5,31 @@ import gotService from '../../services/gotService';
 import {withRouter} from 'react-router-dom';
 
 export class BooksPage extends Component {
-    gotService = new gotService();
+	gotService = new gotService();
 
-    state = {
-        selectedBook: null,
-        error: false
-    }
+	state = {
+		error: false
+	}
 
-    onItemSelected = (id) => {
-        this.setState({
-            selectedBook: id
-        })
-    }
+	componentDidCatch() {
+		this.setState({
+			error: true
+		})
+	}
 
-    componentDidCatch() {
-        this.setState({
-            error: true
-        })
-    }
+	render() {
+		if (this.state.error) {
+			return <ErrorMessage/>
+		}
 
-    render() {
-        if (this.state.error) {
-            return <ErrorMessage/>
-        }
-
-        return (
-            <ItemList 
-            onItemSelected={(itemId) => {
-                this.props.history.push(itemId)
-            }}
-            getData={this.gotService.getAllBooks}
-            renderItem={({name}) => name}/>
-        )
-    }
+		return (
+			<ItemList 
+			onItemSelected={(itemId) => {
+				this.props.history.push(itemId)
+			}}
+			getData={this.gotService.getAllBooks}
+			renderItem={({name}) => name}/>
+		)
+	}
 }
 export default withRouter(BooksPage);
