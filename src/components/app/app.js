b9d8703460724116c677180fb7c966a222d426c0
Igor <ermygut@gmail.com>
2021-03-24 14:05:17 +0200

    new pages and refactoring service

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -4,8 +4,15 @@ import Header from '../header';
 import RandomChar from '../randomChar';
 import ErrorMessage from '../errorMessage';
 import CharacterPage from '../characterPage';
+import ItemList from '../itemList';
+import CharDetails from '../charDetails';
+import gotService from '../../services/gotService';
+
 
 export default class App extends Component {
+	gotService = new gotService();
+
+
 	state = {
 		showRandomChar: true,	
 		error: false
@@ -49,6 +56,26 @@ export default class App extends Component {
 						</Col>
 					</Row>
 					<CharacterPage/>
+					<Row>
+						<Col md='6'>
+							<ItemList 
+							onCharSelected={this.onCharSelected}
+							getData={this.gotService.getAllBooks}/>
+						</Col>
+						<Col md='6'>
+							<CharDetails charId = {this.state.selectedChar} />
+						</Col>
+           			 </Row>				
+					<Row>
+						<Col md='6'>
+							<ItemList 
+							onCharSelected={this.onCharSelected}
+							getData={this.gotService.getAllHouses}/>
+						</Col>
+						<Col md='6'>
+							<CharDetails charId = {this.state.selectedChar} />
+						</Col>
+           			 </Row>
 				</Container>
 			</>
 		);
